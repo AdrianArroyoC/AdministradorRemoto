1a2d9be9b9d1f59ab240c86daff47c2992894bc0
bioazard <rjmultimedia@gmail.com>
2018-05-16 21:33:46 -0500

    Lista interface con try catch y minimizar

--- AdministradorRemotoCliente/src/principal/VentanaCliente.java
+++ AdministradorRemotoCliente/src/principal/VentanaCliente.java
@@ -5,12 +5,90 @@
  */
 package principal;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.InputMismatchException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 /**
  *
- * @author Carlos González <carlos85g at gmail.com>
+ * @author Estudiantes mis Cuvalles.
  */
-public class VentanaCliente extends JFrame{
-    
+public class VentanaCliente extends JFrame {
+
+    JLabel LabelNombre;
+    JLabel LabelApellidos;
+    JLabel LabelCodigo;
+    JLabel LabelIpServidor;
+    JLabel LabelStatus;
+    JLabel LabelEspaciador;
+
+    public void start() {
+        LabelNombre = new JLabel();
+        LabelNombre.setText("Nombre:           ");
+        JTextField TextoNombre = new JTextField(15);
+        LabelApellidos = new JLabel();
+        LabelApellidos.setText("Apellidos:         ");
+        JTextField TextoApellidos = new JTextField(15);
+        LabelCodigo = new JLabel();
+        LabelCodigo.setText("Código:              ");
+        JTextField TextoCodigo = new JTextField(15);
+        LabelIpServidor = new JLabel();
+        LabelIpServidor.setText("Ip del Servidor:");
+        JTextField TextoIpServidor = new JTextField(15);
+        LabelStatus=new JLabel();
+        LabelStatus.setText("Estado: Pendiente del servidor");
+        LabelEspaciador = new JLabel();
+        LabelStatus.setHorizontalAlignment(JTextField.CENTER);
+
+        //TextStatus.setText("Pendiente del servidor");
+        JButton ButtonIniciar = new JButton();
+        ButtonIniciar.setText("Iniciar");
+        ButtonIniciar.setLocation(200, 200);
+        add(LabelNombre);
+        add(TextoNombre);
+        add(LabelApellidos);
+        add(TextoApellidos);
+        add(LabelCodigo);
+        add(TextoCodigo);
+        add(LabelIpServidor);
+        add(TextoIpServidor);
+        add(ButtonIniciar);
+        add(LabelEspaciador);
+        //add(LabelStatus);
+        ButtonIniciar.addActionListener(
+                new ActionListener() {
+
+            public void actionPerformed(
+                    ActionEvent e) {
+
+                try {
+                         JOptionPane.showMessageDialog(null,"no llenaste los datos correctamente!");
+                         setState(JFrame.ICONIFIED);
+                } catch (InputMismatchException ex) {
+                    JOptionPane.showMessageDialog(null, ex.toString(), "Error al conectarse al server",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+
+            }
+
+        }
+        );
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String args[]) {
+        new VentanaCliente().start();
+    }
 }
