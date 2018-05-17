192b46642b2999ffd024efbe5609e7b734e6fec7
Adrian Arroyo Ceja <Adrian.Arroyo@alumno.udg.mx>
2018-05-16 19:45:02 -0500

    Eliminado los comentarios

--- AdministradorRemotoServidor/src/principal/ManejadorConexiones.java
+++ AdministradorRemotoServidor/src/principal/ManejadorConexiones.java
@@ -2,12 +2,6 @@ package principal;
 
 import java.util.ArrayList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Carlos González <carlos85g at gmail.com>
--- AdministradorRemotoServidor/src/principal/ReceptorComandos.java
+++ AdministradorRemotoServidor/src/principal/ReceptorComandos.java
@@ -3,12 +3,6 @@ package principal;
 import java.awt.Robot;
 import java.net.Socket;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Carlos González <carlos85g at gmail.com>
