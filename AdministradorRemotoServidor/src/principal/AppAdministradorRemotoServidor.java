81358500683aee7cf22b580d110e0cce3fd0a56f
Adrian Arroyo Ceja <Adrian.Arroyo@alumno.udg.mx>
2018-05-16 19:33:34 -0500

    Primer commit de prueba

---
+++ AdministradorRemotoServidor/src/principal/AppAdministradorRemotoServidor.java
@@ -0,0 +1,11 @@
+package principal;
+
+/**
+ * @author @adrianarroyoc <adrianarroyoceja@gmail.com>
+ */
+
+public class AppAdministradorRemotoServidor {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
