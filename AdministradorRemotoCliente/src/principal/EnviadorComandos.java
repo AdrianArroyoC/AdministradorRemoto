808d80bdc74d564f2c8f6be323f559bfc2b57c81
bioazard <rjmultimedia@gmail.com>
2018-05-23 22:29:55 -0500

    full screen client

--- AdministradorRemotoCliente/src/principal/EnviadorComandos.java
+++ AdministradorRemotoCliente/src/principal/EnviadorComandos.java
@@ -73,7 +73,8 @@ public class EnviadorComandos implements Runnable, KeyListener, MouseMotionListe
         /* Asignar capturista */
         this.Capturista = new JFrame();
         this.Capturista.setUndecorated(true);
-        this.Capturista.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+        //this.Capturista.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+        this.Capturista.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.Capturista.setSize(servidorAncho, servidorAlto);
         this.Capturista.setResizable(false);
         this.Capturista.setVisible(true);
